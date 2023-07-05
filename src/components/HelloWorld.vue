dd05ccbab3e928e7dd7742062ffd87121bc80651
“fast1ine” <1082hj@gmail.com>
2023-07-06 00:37:01 +0900

    Reset HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,178 +1,113 @@
-<!doctype html>
-<html lang="ko">
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
 
-<head>
-    <meta charset="utf-8">
-    <title>중동중학교 정기고사 시험지 총정리 - 코딩 동아리</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Gothic+A1&display=swap" rel="stylesheet">
-    <style>
-        h1{
-            font-family: 'Black Han Sans', sans-serif;
-            font-size: 400%;
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
     }
-    h2{
-            font-family: 'Gothic A1', sans-serif;
-            font-size: 300%;
-    }
-    h3{
-            font-family: 'Gothic A1', sans-serif;
-            font-size: 250%;
-    }
-    h4{
-            font-family: 'Gothic A1', sans-serif;
-            font-size: 200%;
-    }
-    h5{
-            font-family: 'Gothic A1', sans-serif;
-            font-size: 100%;
-    }
-    hr {
-  height: 3px;
-  background-color: black;
-    }
-    </style>
-</head>
+  }
+}
+</script>
 
-  <body>
-        <h1>중동중학교 정기고사 시험지 총정리!!</h1>
-        <h5>제작자: 30325 이호진 (코딩 동아리 대표)</h5></br>
-        <h5>!!NOTICE!! - 현재는 2022년 자료까지만 지원합니다. 앞으로 사진파일 확인과 온라인 문제풀이, 전체 PDF 파일 지원 계획이고, 오류제보는 1082hj@gmail.com으로 부탁드립니다.</h5>
-        <button type="button" class="btn btn-success" onclick="window.open('http://www.instagram.com/dev_fastline/') ">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
-                <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"></path>
-            </svg>
-            제작자 Instagram
-          </button>
-          <button type="button"  class="btn btn-primary" onclick="window.open('https://www.instagram.com/jdms_prog/') ">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
-                <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"></path>
-            </svg>
-            동아리 Instagram
-          </button></br></br>
-          <hr><hr>
-        <h2>3학년</h2></br>
-        <h3>2023 1학기 기말</h3>
-        <button type="button" class="btn btn-danger" onclick="window.open('http://webtong.kr') ">국어</button>
-        <button type="button" class="btn btn-secondary">영어</button>
-        <button type="button" class="btn btn-warning">수학</button>
-        <button type="button" class="btn btn-info">과학</button>
-        <button type="button" class="btn btn-light">사회</button>
-        <button type="button" class="btn btn-dark">역사 (한국사)</button>
-        </br></br>
-        <hr>
-        </br>
-        <h3>2023 1학기 중간</h3>
-        <button type="button" class="btn btn-danger">국어</button>
-        <button type="button" class="btn btn-secondary">영어</button>
-        <button type="button" class="btn btn-warning">수학</button>
-        <button type="button" class="btn btn-info">과학</button>
-        <button type="button" class="btn btn-light">사회</button>
-        <button type="button" class="btn btn-dark">역사 (한국사)</button>
-        </br></br>
-        <hr>
-        </br>
-        <h3>2022 2학기 기말</h3>
-        <button type="button" class="btn btn-danger">국어</button>
-        <button type="button" class="btn btn-secondary">영어</button>
-        <button type="button" class="btn btn-warning">수학</button>
-        <button type="button" class="btn btn-info">과학</button>
-        <button type="button" class="btn btn-light">사회</button>
-        <button type="button" class="btn btn-dark">역사 (한국사)</button>
-        </br></br>
-        <hr>
-        </br>
-        <h3>2022 2학기 중간</h3>
-        <button type="button" class="btn btn-danger">국어</button>
-        <button type="button" class="btn btn-secondary">영어</button>
-        <button type="button" class="btn btn-warning">수학</button>
-        <button type="button" class="btn btn-info">과학</button>
-        <button type="button" class="btn btn-light">사회</button>
-        <button type="button" class="btn btn-dark">역사 (한국사)</button>
-        </br></br>
-        <hr>
-        </br>
-        <h3>2022 1학기 기말</h3>
-        <button type="button" class="btn btn-danger">국어</button>
-        <button type="button" class="btn btn-secondary">영어</button>
-        <button type="button" class="btn btn-warning">수학</button>
-        <button type="button" class="btn btn-info">과학</button>
-        <button type="button" class="btn btn-light">사회</button>
-        <button type="button" class="btn btn-dark">역사 (한국사)</button>
-        </br></br>
-        <hr>
-        </br>
-        <h3>2022 1학기 중간</h3>
-        <button type="button" class="btn btn-danger">국어</button>
-        <button type="button" class="btn btn-secondary">영어</button>
-        <button type="button" class="btn btn-warning">수학</button>
-        <button type="button" class="btn btn-info">과학</button>
-        <button type="button" class="btn btn-light">사회</button>
-        <button type="button" class="btn btn-dark">역사 (한국사)</button>
-        </br></br>
-        <hr><hr>
-        <h2>2학년</h2></br>
-        <h3>2023 1학기 기말</h3>
-        <button type="button" class="btn btn-danger">국어</button>
-        <button type="button" class="btn btn-secondary">영어</button>
-        <button type="button" class="btn btn-warning">수학</button>
-        <button type="button" class="btn btn-info">과학</button>
-        <button type="button" class="btn btn-dark">역사 (세계사)</button>
-        <button type="button" class="btn btn-success">한문</button>
-        </br></br>
-        <hr>
-        </br>
-        <h3>2023 1학기 중간</h3>
-        <button type="button" class="btn btn-danger">국어</button>
-        <button type="button" class="btn btn-secondary">영어</button>
-        <button type="button" class="btn btn-warning">수학</button>
-        <button type="button" class="btn btn-info">과학</button>
-        <button type="button" class="btn btn-dark">역사 (세계사)</button>
-        </br></br>
-        <hr>
-        </br>
-        <h3>2022 2학기 기말</h3>
-        <button type="button" class="btn btn-danger">국어</button>
-        <button type="button" class="btn btn-secondary">영어</button>
-        <button type="button" class="btn btn-warning">수학</button>
-        <button type="button" class="btn btn-info">과학</button>
-        <button type="button" class="btn btn-dark">역사 (세계사)</button>
-        <button type="button" class="btn btn-success">한문</button>
-        </br></br>
-        <hr>
-        </br>
-        <h3>2022 2학기 중간</h3>
-        <button type="button" class="btn btn-danger">국어</button>
-        <button type="button" class="btn btn-secondary">영어</button>
-        <button type="button" class="btn btn-warning">수학</button>
-        <button type="button" class="btn btn-info">과학</button>
-        <button type="button" class="btn btn-dark">역사 (세계사)</button>
-        </br></br>
-        <hr>
-        </br>
-        <h3>2022 1학기 기말</h3>
-        <button type="button" class="btn btn-danger">국어</button>
-        <button type="button" class="btn btn-secondary">영어</button>
-        <button type="button" class="btn btn-warning">수학</button>
-        <button type="button" class="btn btn-info">과학</button>
-        <button type="button" class="btn btn-dark">역사 (세계사)</button>
-        <button type="button" class="btn btn-success">한문</button>
-        </br></br>
-        <hr>
-        </br>
-        <h3>2022 1학기 중간</h3>
-        <button type="button" class="btn btn-danger">국어</button>
-        <button type="button" class="btn btn-secondary">영어</button>
-        <button type="button" class="btn btn-warning">수학</button>
-        <button type="button" class="btn btn-info">과학</button>
-        <button type="button" class="btn btn-dark">역사 (세계사)</button>
-        </br></br>
-        <hr>
-        </br>
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-  </body>
-</html>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
